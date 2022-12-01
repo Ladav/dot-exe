6d3225d0b2ab495977e8b5b9bcd347bb12b37495
Ladav <davindersaini003@gmail.com>
2022-12-01 22:55:12 +0530

    feat: setup global validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from './prisma/prisma.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,16 @@ async function bootstrap() {
   // close app on prisma shutdown signal
   const prisma = app.get(PrismaService);
   await prisma.enableShutdownHooks(app);
+
+  // setup global validations
+  // validates the correctness of any data received in an incoming request
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
 
   await app.listen(3001);
 }
